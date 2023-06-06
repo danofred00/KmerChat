19890f27114185d81ed3d63d5ec83477483b38d1
Daniel Leussa <leussadaniel04@gmail.com>
2023-06-06 21:45:49 +0100

    better main.cpp file for client

--- client/main.cpp
+++ client/main.cpp
@@ -1,40 +1,19 @@
 
-#include <QCoreApplication>
+#include <QApplication>
 
 #include <QDebug>
 
-#include "core/user.h"
-#include "serversocket.h"
-#include "services/authservice.h"
+#include "appclient.h"
 
 using namespace Client;
-using namespace Client::Service;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication app(argc, argv);
+    QApplication app(argc, argv);
 
-    auto user = Core::User::Builder()
-        .hasUsername("admin")
-        .hasPassword("password")
-        .build();
+    AppClient client;
 
-    ServerSocket socket = ServerSocket();
-
-    socket.open(QUrl("ws://localhost:1234"));
-
-    QObject::connect(&socket, &ServerSocket::connected, [&]() {
-
-        AuthService::start(&socket);
-
-        AuthService * auth = AuthService::instance();
-
-        auth->setUser(&user);
-
-        auth->login();
-
-    });
-
+    client.start("localhost", 1234);
 
     return app.exec();
 }
